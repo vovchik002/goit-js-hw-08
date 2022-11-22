4ad83867d8b91d8247fad62fad33d3d93f6a2559
vovchik002 <106514220+vovchik002@users.noreply.github.com>
2022-11-22 23:39:34 +0000

    Deploying to gh-pages from @ vovchik002/goit-js-hw-08@723e3afd1e8ff3a1c80178a0242f02780c1b83c5 🚀

---
+++ 02-video.b87ecf10.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("iframe"),o=new Vimeo.Player(e);o.on("play",(function(){console.log("played the video!")})),o.getVideoTitle().then((function(e){console.log("title:",e)}));
+//# sourceMappingURL=02-video.b87ecf10.js.map
