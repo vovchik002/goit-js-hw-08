7c4616ab6b45b72cc951d103bef0a6054d6575a1
vovchik002 <106514220+vovchik002@users.noreply.github.com>
2022-11-23 23:40:36 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
+const refs = {
+formRef: document.querySelector('.feedback-form '), 
+emailRef:document.querySelector('.feedback-form input'),
+textareaRef:document.querySelector('.feedback-form textarea'),
+}
+refs.formRef.addEventListener('input', throttle(onInput,500))
+refs.formRef.addEventListener('submit', onSubmit)
+populateTextatera() 
 
+function onInput(event) {
+  const textEmailTextatera = {}
+  textEmailTextatera.email = refs.emailRef.value
+  textEmailTextatera.textarea=refs.textareaRef.value
+  localStorage.setItem("feedback-form-state", JSON.stringify(textEmailTextatera))
+ 
+ }
+function onSubmit(event) {
+  event.preventDefault()
+  event.currentTarget.reset() 
+  localStorage.removeItem("feedback-form-state")
+  
+}
+function populateTextatera() {
+const saveMassage = localStorage.getItem("feedback-form-state")
+  if (saveMassage) {
+let valueInTexT=JSON.parse(saveMassage)
+    refs.textareaRef.value = valueInTexT.textarea
+    refs.emailRef.value = valueInTexT.email
+   console.log(valueInTexT)
+  }
+ 
+ }
